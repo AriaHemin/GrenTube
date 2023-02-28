7aea2d44e14d93b94da94aa4566c1916cc295295
Aria Hemin <ariahemin@gmail.com>
2023-02-28 11:40:40 +0300

    fixed header styling

--- src/components/landingPageHeader.jsx
+++ src/components/landingPageHeader.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 function LandingPageHeader (props) {
   return (
     <div>
-      <header className='flex flex-row justify-between w-screen items-center' >
+      <header className='flex flex-row justify-between w-screen items-center ' >
         <div className='logo' >
-          <img className='w-[140px] h-[50px]' src='https://firebasestorage.googleapis.com/v0/b/grentube-66fd4.appspot.com/o/grentube%20logo.svg?alt=media&token=6aae336f-70c4-4f0f-9494-305831bfb77e' alt='GrenTube'/>
+          <img className='w-0 xs:w-[50px] object-cover object-left h-[50px] sm:w-[140px]' src='https://firebasestorage.googleapis.com/v0/b/grentube-66fd4.appspot.com/o/grentube%20logo.svg?alt=media&token=6aae336f-70c4-4f0f-9494-305831bfb77e' alt='GrenTube'/>
         </div>
         <div className='font-bold flex flex-row items-center mx-1' >
           <Link to='/'>
@@ -22,7 +22,7 @@ function LandingPageHeader (props) {
           </Link>
           <div className='px-2' >
             <Link to='/dashboard'>
-              <button >start a session</button>
+              <button className=' text-[0.7em] md:text-[1em]' >start a session</button>
             </Link>
           </div>
         </div>
